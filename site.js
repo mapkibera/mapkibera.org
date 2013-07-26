674f2e13fc6574bc1d49452accee4daf0c8f94f6
Mikel Maron <mikel@HummingBird.(none)>
2013-07-26 08:49:09 -0400

    initial changes, just for sharing

--- site.js
+++ site.js
@@ -1,3 +1,36 @@
 ---
 ---
 ;{% include js/jquery.min.js %}
+
+;(function(context) {
+
+    var mapkibera = {}
+    mapkibera.slideShow = function(context) {
+        var slideIndex = 1,
+            $slide = $('[data-index]', context),
+            slides = $slide.length;
+
+        $('a.slide-control').click(function() {
+            $slide.removeClass('active');
+            if ($(this).hasClass('next')) {
+                if (slideIndex >= slides) {
+                    slideIndex = 1;
+                } else {
+                    slideIndex++;
+                }
+            } else {
+                if (slideIndex <= 1) {
+                    slideIndex = slides;
+                } else {
+                    slideIndex--;
+                }
+            }
+
+           $('[data-index="slide-' + slideIndex + '"]', context).addClass('active');
+            return false;
+        });
+    };
+
+   window.mapkibera = mapkibera;
+})(window);
+
