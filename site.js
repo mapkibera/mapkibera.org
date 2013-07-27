24c567e01432e63f045240832dda97bced67cb2b
Mikel Maron <mikel@HummingBird.(none)>
2013-07-27 12:13:48 -0400

    another bunch of work. still very much in progress

--- site.js
+++ site.js
@@ -5,6 +5,20 @@
 ;(function(context) {
 
     var mapkibera = {}
+
+    mapkibera.global = function() {
+        function scroll(el){
+            $('html, body').animate({
+                scrollTop: $(el).offset().top
+            }, 500);
+        }
+
+        $('a[data-scroll]').click(function() {
+            var to = $(this).attr('href');
+            scroll(to);
+            return false;
+        });
+    };
     mapkibera.slideShow = function(context) {
         var slideIndex = 1,
             $slide = $('[data-index]', context),
@@ -29,6 +43,12 @@
            $('[data-index="slide-' + slideIndex + '"]', context).addClass('active');
             return false;
         });
+    };
+
+    mapkibera.map = function(context) {
+      var map = L.map(context.id).setView([parseFloat(context.lat), parseFloat(context.lon)], parseInt(context.zoom));
+      L.tileLayer(context.tileserver + '{z}/{x}/{y}.png', {
+      }).addTo(map);
     };
 
    window.mapkibera = mapkibera;
