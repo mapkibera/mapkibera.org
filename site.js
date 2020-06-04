56d8b215e6d1eb6d46803b0a16ab1655bab27c9c
Mikel Maron <mikel_maron@yahoo.com>
2020-06-03 21:53:01 -0400

    switching to new imagery switcher

--- site.js
+++ site.js
@@ -46,40 +46,86 @@
     };
 
     mapkibera.map = function(context) {
-      if (context.zoom_control == 'hide') {
-        var map = L.map(context.id, {zoomControl:false}).setView([parseFloat(context.lat), parseFloat(context.lon)], parseInt(context.zoom));
-        map.attributionControl.setPrefix(false);
+      if (context.mapbox_accessToken) {
+        mapkibera.mapbox_map(context);
       } else {
-        var map = L.map(context.id).setView([parseFloat(context.lat), parseFloat(context.lon)], parseInt(context.zoom));
-        map.attributionControl.setPrefix(false).addAttribution("&copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, Map Kibera");
+        mapkibera.leaflet_map(context);
       }
-
-      if (context.layer_control == 'show') {
-        //var layer_control = L.control.layers().addTo(map);
-        var layers = {};
-      }
-      var tileservers = context.tileserver.split(',');
-      for (var i=0; i< tileservers.length; i++) {  
-        if (context.layer_control == 'show') {
-          layer_name_array = tileservers[i].split('/');
-          layer_name = layer_name_array[ layer_name_array.length -2 ];
-          layers[ layer_name ] = L.tileLayer(tileservers[i] + '{z}/{x}/{y}.png', { });
-          if (i == 0) {
-            layers[ layer_name ].addTo(map);
-          }
-        } else {
-          L.tileLayer(tileservers[i] + '{z}/{x}/{y}.png', {
-          }).addTo(map);
-        }
-      }
-      if (context.layer_control == 'show') {
-        L.control.layers(layers).addTo(map);
-      }
-
-      map.scrollWheelZoom.disable();
     };
 
+   mapkibera.mapbox_map = function(context) {
+
+     mapboxgl.accessToken = context.mapbox_accessToken;
+     var map = new mapboxgl.Map({
+         container: 'map',
+         style: 'mapbox://styles/mapkibera/ck1fidzbc29at1crte4b8k8mw',
+         zoom: 15,
+         center: [36.79164, -1.31234]
+     });
+
+     var toggleableLayerIds = ['nairobi-composite-60s', 'january-2007', 'august-2008', 'july-2009', '2018'];
+
+     for (var i = 0; i < toggleableLayerIds.length; i++) {
+         var id = toggleableLayerIds[i];
+
+         var link = document.createElement('a');
+         link.id = id;
+         link.href = '#';
+         link.className = '';
+         link.textContent = id;
+
+         link.onclick = function (e) {
+             var clickedLayer = this.textContent;
+             e.preventDefault();
+             e.stopPropagation();
+
+             toggleableLayerIds.forEach((layerId) => {
+                 var opacity = clickedLayer === layerId ? 1 : 0;
+                 var className = clickedLayer === layerId ? 'active' : '';
+                 document.getElementById(layerId).className = className;
+                 map.setPaintProperty(layerId, 'raster-opacity', opacity);
+             });
+         };
+
+         var layers = document.getElementById('mapbox-menu');
+         layers.appendChild(link);
+     }
+
+   };
+
+   mapkibera.leaflet_map = function(context) {
+     if (context.zoom_control == 'hide') {
+       var map = L.map(context.id, {zoomControl:false}).setView([parseFloat(context.lat), parseFloat(context.lon)], parseInt(context.zoom));
+       map.attributionControl.setPrefix(false);
+     } else {
+       var map = L.map(context.id).setView([parseFloat(context.lat), parseFloat(context.lon)], parseInt(context.zoom));
+       map.attributionControl.setPrefix(false).addAttribution("&copy; <a href='http://openstreetmap.org'>OpenStreetMap</a> contributors, Map Kibera");
+     }
+
+     if (context.layer_control == 'show') {
+       //var layer_control = L.control.layers().addTo(map);
+       var layers = {};
+     }
+     var tileservers = context.tileserver.split(',');
+     for (var i=0; i< tileservers.length; i++) {
+       if (context.layer_control == 'show') {
+         layer_name_array = tileservers[i].split('/');
+         layer_name = layer_name_array[ layer_name_array.length -2 ];
+         layers[ layer_name ] = L.tileLayer(tileservers[i] + '{z}/{x}/{y}.png', { });
+         if (i == 0) {
+           layers[ layer_name ].addTo(map);
+         }
+       } else {
+         L.tileLayer(tileservers[i] + '{z}/{x}/{y}.png', {
+         }).addTo(map);
+       }
+     }
+     if (context.layer_control == 'show') {
+       L.control.layers(layers).addTo(map);
+     }
+
+     map.scrollWheelZoom.disable();
+   };
 
    window.mapkibera = mapkibera;
 })(window);
-
